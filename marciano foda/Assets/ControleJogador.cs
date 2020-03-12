a2cff7fb59850a41f51d495c2f4d83b083545eff
JackFrost260 <44248464+JackFrost260@users.noreply.github.com>
2020-03-12 10:03:14 -0300

    Commit inicial

---
+++ marciano foda/Assets/ControleJogador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleJogador : MonoBehaviour
+{
+
+    public float sensibilidadeMouse;
+
+    public Transform modeloJogador;
+
+    float rotacaoX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidadeMouse * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidadeMouse * Time.deltaTime;
+
+        rotacaoX -= mouseY;
+        rotacaoX = Mathf.Clamp(rotacaoX, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(rotacaoX, 0f, 0f);
+        modeloJogador.Rotate(Vector2.up * mouseX);
+    }
+}
---
+++ marciano foda/Assets/MovimentoDoJogador.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoDoJogador : MonoBehaviour
+{
+    CharacterController controlador;
+    public float velocidade = 15f;
+    public float gravidade = -9.81f;
+    public Transform checagemChao;
+    public float distanciaChao = 0.5f;
+    public float distanciaPulo = 20f;
+    public LayerMask mascaraChao;
+
+    Vector3 velocidadeDeQueda;
+    Vector3 velocidadePulo;
+    bool noChao;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlador = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        noChao = Physics.CheckSphere(checagemChao.position, distanciaChao, mascaraChao);
+
+        if(noChao && velocidadeDeQueda.y < 0)
+        {
+            velocidadeDeQueda.y = -1f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            z = 1 * distanciaPulo;
+        }
+
+        Vector3 movimento = transform.right * x + transform.forward * y;
+        controlador.Move(movimento * velocidade * Time.deltaTime);
+        velocidadeDeQueda.y += gravidade * Time.deltaTime;
+        velocidadePulo.y = distanciaPulo * Time.deltaTime * z;
+        
+
+
+        controlador.Move(velocidadeDeQueda * Time.deltaTime);
+        controlador.Move(Time.deltaTime * velocidadePulo);
+
+    }
+}
