a130be1d7e0370f0d688b3c62c8fb4bd5d5446ea
JackFrost260 <44248464+JackFrost260@users.noreply.github.com>
2020-04-22 10:50:49 -0300

    Menu inicial funcionando!

---
+++ marciano foda/Assets/Menu Assets/Scripts/buttonSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonSetup : MonoBehaviour
+{
+   
+    public GameObject weather1;
+
+    public void startGame()
+    {
+        Destroy(weather1);
+        SceneManager.LoadScene("Level_Design_V1", LoadSceneMode.Single);
+    }
+    
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
