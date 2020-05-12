edef11f6cb9420a9ccd4c2acd3e375191988c8f8
Gil788 <gildineia.vieira@gmail.com>
2020-05-12 10:26:11 -0300

    .

--- marciano foda/Assets/Scripts/Crafting.cs
+++ marciano foda/Assets/Scripts/Crafting.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Policy;
+//using System.Security.Policy;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
